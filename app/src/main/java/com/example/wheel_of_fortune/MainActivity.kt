e97692df464f772d3b6aeb7a6bb9fa0f8c7bb199
Mads Nielsen <90190656+Zetles96@users.noreply.github.com>
2022-11-21 18:47:01 +0100

    made some components

--- app/src/main/java/com/example/wheel_of_fortune/MainActivity.kt
+++ app/src/main/java/com/example/wheel_of_fortune/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.wheel_of_fortune
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.wheel_of_fortune.ui.theme.WheeloffortuneTheme
 
@@ -18,29 +22,48 @@ class MainActivity : ComponentActivity() {
         setContent {
             WheeloffortuneTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                WheelOfFortuneApp()
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun WheelOfFortuneApp() {
+    Column(modifier = Modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        WelcomeMessage(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentSize(Alignment.Center)
+        )
+        SpinTheWheelButton(modifier = Modifier.fillMaxWidth().wrapContentSize(Alignment.Center))
+    }
+
+}
+
+@Composable
+fun WelcomeMessage(modifier: Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = stringResource(R.string.welcome_to_the_wheel_of_fortune),
+        modifier = modifier.wrapContentWidth(Alignment.CenterHorizontally)
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    WheeloffortuneTheme {
-        Greeting("Android")
+fun WheelPicture(modifier: Modifier) {
+
+}
+
+@Composable
+fun SpinTheWheelButton(modifier: Modifier) {
+    Button(onClick = { /*TODO*/ }) {
+        Text(text = stringResource(R.string.spin_button_text))
+    }
+}
+@Composable
+fun SubmitLetterButton(modifier: Modifier) {
+    Button(onClick = { /*TODO*/ }) {
+        Text(text = stringResource(R.string.spin_button_text))
     }
 }
