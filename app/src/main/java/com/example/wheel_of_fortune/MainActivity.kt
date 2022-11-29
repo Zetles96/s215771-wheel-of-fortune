5a0c6a548a1aacc2eeab93693fd452d972fa5cf5
Mads Nielsen <90190656+Zetles96@users.noreply.github.com>
2022-11-29 19:48:18 +0100

    some basic ui i made, the rest will be handled when implementing logic

--- app/src/main/java/com/example/wheel_of_fortune/MainActivity.kt
+++ app/src/main/java/com/example/wheel_of_fortune/MainActivity.kt
@@ -3,16 +3,15 @@ package com.example.wheel_of_fortune
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.wheel_of_fortune.ui.theme.WheeloffortuneTheme
 
@@ -31,23 +30,30 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun WheelOfFortuneApp() {
+
     Column(modifier = Modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        WelcomeMessage(
+        PlayerInfo(modifier = Modifier)
+        SpinTheWheelButton(
             modifier = Modifier
                 .fillMaxWidth()
                 .wrapContentSize(Alignment.Center)
         )
-        SpinTheWheelButton(modifier = Modifier.fillMaxWidth().wrapContentSize(Alignment.Center))
+        GuessAndSubmitLetter(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentSize(Alignment.Center)
+        )
     }
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun WelcomeMessage(modifier: Modifier) {
-    Text(
-        text = stringResource(R.string.welcome_to_the_wheel_of_fortune),
-        modifier = modifier.wrapContentWidth(Alignment.CenterHorizontally)
-    )
+fun PlayerInfo(modifier: Modifier) {
+    Column(modifier = Modifier) {
+        Text(text = stringResource(R.string.life))
+        Text(text = stringResource(R.string.points))
+    }
 }
 
 @Composable
@@ -61,9 +67,32 @@ fun SpinTheWheelButton(modifier: Modifier) {
         Text(text = stringResource(R.string.spin_button_text))
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SubmitLetterButton(modifier: Modifier) {
-    Button(onClick = { /*TODO*/ }) {
-        Text(text = stringResource(R.string.spin_button_text))
+fun GuessAndSubmitLetter(modifier: Modifier) {
+    val textOnlyRegex = remember { Regex("[A-Z]*") }
+    Row(modifier = Modifier) {
+        var text by remember { mutableStateOf("") }
+
+        OutlinedTextField(
+            value = text,
+            maxLines = 1,
+            onValueChange = {
+                if (it.matches(textOnlyRegex) && it.length <= 1) {
+                    text = it.uppercase()
+                }
+            },
+            label = { Text(text = stringResource(R.string.input)) },
+            placeholder = { Text(text = stringResource(R.string.writeLetter)) },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                capitalization = KeyboardCapitalization.Characters
+            ),
+        )
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = stringResource(R.string.submit))
+        }
     }
+
 }
